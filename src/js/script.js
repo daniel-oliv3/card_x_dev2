95dbbe6ddd21098c14bd0ef5a8558e786077a88b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-06-14 11:02:16 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+console.log(document.getElementById("btn-avancar"));             
 
 
 
@@ -21,3 +22,52 @@
 
 
 
+
+
+
+
+
+
+
+
+/**/
+// alert("Olá, Mundo!");
+// window.alert("Olá, Mundo!");
+
+/*
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
+cartao da lista
+  
+passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+passo 3 - fazer aparecer o próximo cartão da lista
+passo 4 - buscar o cartão que esta selecionado e esconder
+
+
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
+anterior da lista
+
+passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+passo 3 - fazer aparecer o cartão anterior da lista
+passo 4 - buscar o cartão que esta selecionado e esconder
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
