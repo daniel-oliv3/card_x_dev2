ed4455bb0e38571f23d762fab741643dad65c1fc
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-06-15 10:22:34 -0300

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,59 @@
-console.log(document.getElementById("btn-avancar"));             
+/* OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
+cartao da lista */
+
+// passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+const btnAvancar = document.getElementById("btn-avancar");
+// passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+
+const cartoes = document.querySelectorAll(".cartao");
+
+// passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+btnAvancar.addEventListener("click", function (){
+    // console.log("Cliquei!");
+    
+    // console.log(cartaoAtual);
+    // console.log(cartoes.length - 1);
+    // console.log(cartaoAtual === cartoes.length - 1);
+    
+    if(cartaoAtual === cartoes.length - 1) return;
+
+    // passo 4 - buscar o cartão que esta selecionado e esconder
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+    // passo 3 - fazer aparecer o próximo cartão da lista, colocando a classe selecionado nele
+    // const cartoes = document.querySelectorAll(".cartao");
+    // console.log(cartoes[1]);
+    // console.log(cartoes[1].classList);
+    cartaoAtual++;
+    // console.log(cartaoAtual);
+    // console.log(cartoes[cartaoAtual]);
+    cartoes[cartaoAtual].classList.add("selecionado");
 
 
 
+});
 
 
+/* OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
+anterior da lista */
 
+// passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+btnVoltar.addEventListener("click", function (){
 
+    console.log(cartaoAtual);
+    if(cartaoAtual === 0) return;
 
+    // passo 4 - buscar o cartão que esta selecionado e esconder
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
 
-
-
-
-
-
-
+    // passo 3 - fazer aparecer o cartão anterior da lista
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add("selecionado");
+});
 
 
 
@@ -33,6 +73,7 @@ console.log(document.getElementById("btn-avancar"));
 /**/
 // alert("Olá, Mundo!");
 // window.alert("Olá, Mundo!");
+// console.log(document.getElementById("btn-avancar"));             
 
 /*
 OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
